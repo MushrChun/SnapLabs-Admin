0807a3a7a7f0c5d978c00a2c2fcbf72b0597371f
Chun Shen <mushrchun@gmail.com>
2017-10-03 16:10:40 +1100

    create routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {MatButtonModule} from '@angular/material';
-import {MatSidenavModule} from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material';
-import {MatIconModule} from '@angular/material';
-import {MatListModule} from '@angular/material';
+import { RouterModule } from '@angular/router';
+
+import { MatButtonModule } from '@angular/material';
+import { MatSidenavModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+import { MatListModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatMenuModule} from '@angular/material';
+import { MatMenuModule } from '@angular/material';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersComponent } from './users/users.component';
+import { InvestigationsComponent } from './investigations/investigations.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    UsersComponent,
+    InvestigationsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -24,9 +34,35 @@ import {MatMenuModule} from '@angular/material';
     MatIconModule,
     MatListModule,
     FlexLayoutModule,
-    MatMenuModule
+    MatMenuModule,
+    RouterModule.forRoot([
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'users',
+        component: UsersComponent
+      },
+      {
+        path: 'investigations',
+        component: InvestigationsComponent
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
+
 export class AppModule { }
